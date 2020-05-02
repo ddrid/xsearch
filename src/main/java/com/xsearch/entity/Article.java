abe7c44065f7a3b5d79d7ef6e15ecdada875803c
ddrid <2821566237@qq.com>
2020-05-02 22:19:14 +0800

    -

--- src/main/java/com/xsearch/entity/Article.java
+++ src/main/java/com/xsearch/entity/Article.java
@@ -1,44 +1,22 @@
 package com.xsearch.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
 
-    public Article() {
-    }
-
-    public Article(int index, String title, String content) {
-        this.index = index;
-        this.title = title;
-        this.content = content;
-    }
-
     int index;
+
+    int updateTime;
+
+    String url;
 
     String title;
 
     String content;
 
-    public int getIndex() {
-        return index;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
